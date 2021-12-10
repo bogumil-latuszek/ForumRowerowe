abafecc2fe69ebd20793325d3f79935ea8dde57d
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2021-12-10 09:00:58 +0100

    dodanie dbset do dbcontext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ForumRowerowe.Models;
 
 namespace ForumRowerowe.Data
 {
@@ -12,5 +13,6 @@ namespace ForumRowerowe.Data
             : base(options)
         {
         }
+        public DbSet<Post> Posts { get; set; }
     }
 }
