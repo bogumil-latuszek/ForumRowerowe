c7a28ddf918420c388a4fc4d30894de53fdbe5d3
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2021-12-10 09:02:25 +0100

    autowygenerowanie migracji i bazy danych

--- Data/Migrations/20211210075730_initial_setup.Designer.cs
+++ Data/Migrations/20211210075730_initial_setup.Designer.cs
@@ -4,14 +4,16 @@ using ForumRowerowe.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ForumRowerowe.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211210075730_initial_setup")]
+    partial class initial_setup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
